18561811fe5b7c1b752d2c9f838e25b14af17e48
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-17 20:59:53 -0300

    Fundamentar - js - Tipos dinâmicos

--- 02-trilha-fundamentar/11-guia-javascript/variaveis.js
+++ 02-trilha-fundamentar/11-guia-javascript/variaveis.js
@@ -3,3 +3,10 @@ console.log(clima)
 
 let estado = 'PE'
 console.log(estado)
+console.log(typeof estado)
+
+let isBoolean = false
+console.log(typeof isBoolean)
+
+isBoolean = true
+console.log(isBoolean)
