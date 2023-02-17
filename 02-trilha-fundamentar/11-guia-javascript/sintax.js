9134cd1fbaf7f35ac87566828bac3a9cdfdab8c8
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-17 19:55:50 -0300

    Fundamentar - js - add js

--- 02-trilha-fundamentar/11-guia-javascript/sintax.js
+++ 02-trilha-fundamentar/11-guia-javascript/sintax.js
@@ -1,3 +1,6 @@
 console.log('----------------')
 console.log('Estudando JS')
 console.log('----------------')
+
+// Adicionando um texto no body via javascript
+document.querySelector('body').innerText = 'Estudando JS'
