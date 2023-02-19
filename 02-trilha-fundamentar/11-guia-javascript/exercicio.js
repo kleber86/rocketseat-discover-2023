4a84eb6e8854e5bf58e98e3257f8872da48e44e5
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-19 11:03:25 -0300

    Fundamentar - js - exercicios

---
+++ 02-trilha-fundamentar/11-guia-javascript/exercicio.js
@@ -0,0 +1,50 @@
+// 1
+let weight
+
+// 2
+console.log(typeof weight)
+
+// 3
+let name = 'kleber'
+let age = 10
+let stars = 4.1
+let isSubscribed = true
+
+// 4
+let student = {
+  name: 'kleber',
+  age: 50,
+  weight: 80.1,
+  isSubscribed: true
+}
+
+console.log(typeof student)
+console.log(student)
+console.log(
+  `${student.name} tem ${student.age} de idade e pesa ${student.weight} kg.`
+)
+
+// 5
+let students = []
+
+// 6
+students = [student]
+console.log(students)
+
+// 7
+console.log(students[0])
+
+// 8
+const john = {
+  name: 'john',
+  age: 30,
+  weight: 90,
+  isSubscribed: true
+}
+console.log(students[0])
+console.log(students)
+
+// 9
+var a
+console.log(a)
+a = 1
