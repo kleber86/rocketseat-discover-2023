285bb47010b1d2624482d3f324c387a493a838dc
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-19 11:31:43 -0300

    Fundamentar - js - Retornando valores dentro da função

--- 02-trilha-fundamentar/11-guia-javascript/functions/functions.js
+++ 02-trilha-fundamentar/11-guia-javascript/functions/functions.js
@@ -1,10 +1,17 @@
 // function expression
 // fuction anonymous
 
+let total = 0
 // parâmetros (parameters)
 const sum = function (number1, number2) {
-  console.log(number1 + number2)
+  let total = number1 + number2
+  return total
 }
 
-sum(2, 3) // arguments/argumentos
+let number1 = 33
+let number2 = 29
 
+// console.log(`o numero 1 é ${number1}`)
+// console.log(`o numero 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
+console.log(total)
