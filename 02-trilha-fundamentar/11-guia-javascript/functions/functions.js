1aaf89d282d61cbcde86c1d6af029aab4cc88506
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-22 19:03:42 -0300

    Fundamentar - js - Outra maneira de entender funções

--- 02-trilha-fundamentar/11-guia-javascript/functions/functions.js
+++ 02-trilha-fundamentar/11-guia-javascript/functions/functions.js
@@ -1,17 +1,9 @@
-// function expression
-// fuction anonymous
+// Função é um liquidificador
 
-let total = 0
-// parâmetros (parameters)
-const sum = function (number1, number2) {
-  let total = number1 + number2
-  return total
+function fazerSuco(fruta1, fruta2) {
+  return 'sudo de: ' + fruta1 + fruta2
 }
 
-let number1 = 33
-let number2 = 29
+const copo = fazerSuco('banana', 'maçã')
 
-// console.log(`o numero 1 é ${number1}`)
-// console.log(`o numero 2 é ${number2}`)
-console.log(`a soma é ${sum(number1, number2)}`)
-console.log(total)
+console.log(copo)
