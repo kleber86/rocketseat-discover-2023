8f877dd8332f689a38b979ed5055a0a9e45bc1a0
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-22 22:55:32 -0300

    Fundamentar - js - Funções construtoras

--- 02-trilha-fundamentar/11-guia-javascript/functions/functions.js
+++ 02-trilha-fundamentar/11-guia-javascript/functions/functions.js
@@ -1,15 +1,18 @@
-// callback function
+/**
+ * Function() constructor
+ *
+ * expressão new
+ * criar um novo objeto
+ * this keyword
+ */
 
-function sayMyName(name) {
-  console.log('antes de executar a função callback')
-
-  name()
-
-  console.log('depois de executar o callback')
-}
-
-sayMyName(
-  ()=> {
-    console.log('estou em um callback')
+function Person(name) {
+  this.name = name
+  this.walk = function () {
+    return this.name + ' está andando.'
   }
-)
+}
+const kleber = new Person('Kleber')
+const alves = new Person('Alves')
+console.log(kleber.walk())
+console.log(alves.walk())
