8e14bb4f673980d79973a902f954fdd8f7d74d52
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-24 19:36:33 -0300

    Fundamentar - js - Expressões e operadores

---
+++ 02-trilha-fundamentar/11-guia-javascript/expressoes-operadores.js
@@ -0,0 +1,13 @@
+let number = 1 // Aqui o ; faz diferença
+
+;(function () {
+  console.log('alow')
+})
+
+let number2 = 1
+console.log(++number2)
+
+number2 = 1
+console.log(--number2)
+
+console.log(false ? 'alow' : 'nada')
