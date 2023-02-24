8a4d95878a61f3fdf30f392c8e7f50baa4a1e566
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-24 19:44:37 -0300

    Fundamentar - js - Operadores aritméticos

---
+++ 02-trilha-fundamentar/11-guia-javascript/oper-aritimeticos.js
@@ -0,0 +1,29 @@
+// Operadores Aritiméticos
+
+// multiplicação
+console.log(2.2 * 1)
+
+// divisão
+console.log(10 / 2)
+
+// soma
+console.log(32 + 2)
+
+// subtração
+console.log(1 - 1)
+
+// resto da divisão
+console.log(11 % 2)
+
+// incremento
+let incremento = 0
+incremento++
+console.log(incremento)
+
+// decremento
+let decremento = 0
+--decremento
+console.log(decremento)
+
+// exponencial
+console.log(3 ** 3)
