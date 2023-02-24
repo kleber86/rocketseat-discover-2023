05eb5401bf678502616a37568099a68d141b4217
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-23 22:19:17 -0300

    Fundamentar - js - Casas decimais

---
+++ 02-trilha-fundamentar/11-guia-javascript/casa-decimais.js
@@ -0,0 +1,8 @@
+/**
+ * MAnupulando String e Números
+ *
+ * Transformar um número quebrado com 2 casas decimais e trocar ponto por vírgula
+ */
+
+let number = 31213.321321
+console.log(number.toFixed(2).replace('.', ','))
