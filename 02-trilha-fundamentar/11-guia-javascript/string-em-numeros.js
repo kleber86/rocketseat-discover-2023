e91e89c5bbd95e33dc39f0f0becd83486d0ec301
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-23 22:06:16 -0300

    Fundamentar - js - Strings em números

---
+++ 02-trilha-fundamentar/11-guia-javascript/string-em-numeros.js
@@ -0,0 +1,11 @@
+/**
+ * Manipulando Strings e Números
+ *
+ * Transformar String em Número e Número em String
+ */
+
+let string = '123'
+console.log(Number(string))
+
+let numero = 321
+console.log(String(numero))
