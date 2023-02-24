3bdd3793bb89947ed5df7bb8ec001470319a2f47
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-23 22:24:39 -0300

    Fundamentar - js - Separando strings

---
+++ 02-trilha-fundamentar/11-guia-javascript/separando-strings.js
@@ -0,0 +1,5 @@
+let phrase = 'Eu quero viver o Amor!'
+let myArray = phrase.split(' ')
+console.log(myArray) // [ 'Eu quer', ' viver ', ' Am', 'r!' ]
+let phraseWithUnderscore = myArray.join('_')
+console.log(phraseWithUnderscore)
