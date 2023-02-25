4e52fd761d35fd89c61f73dc212f554638e220dc
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-25 15:39:34 -0300

    Fundamentar - js - Falsy e truthy

---
+++ 02-trilha-fundamentar/11-guia-javascript/falsy-truthy.js
@@ -0,0 +1,13 @@
+console.log(true ? 'verdadeiro' : 'falso')
+
+console.log(false ? 'verdadeiro' : 'falso')
+console.log(0 ? 'verdadeiro' : 'falso')
+console.log(undefined ? 'verdadeiro' : 'falso')
+console.log(NaN ? 'verdadeiro' : 'falso')
+
+console.log(true ? 'verdadeiro' : 'falso')
+console.log({} ? 'verdadeiro' : 'falso')
+console.log(1 ? 'verdadeiro' : 'falso')
+console.log(Infinity ? 'verdadeiro' : 'falso')
+console.log('0' ? 'verdadeiro' : 'falso')
+
