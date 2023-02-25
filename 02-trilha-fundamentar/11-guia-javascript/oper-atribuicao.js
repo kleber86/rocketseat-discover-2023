5b3f18441a6999e86ab00651f2b8784606619493
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-25 10:56:25 -0300

    Fundamentar - js - Operadores de atribuição

---
+++ 02-trilha-fundamentar/11-guia-javascript/oper-atribuicao.js
@@ -0,0 +1,26 @@
+let x
+
+// assignment
+x = 1
+
+// addition assignment
+x = x + 2
+//x += 2
+console.log(x)
+
+// subtraction assignment
+x -= 1
+console.log(x)
+
+// multiplication assignment
+x *= 2
+console.log(x)
+
+// division assignment
+x /= 2
+console.log(x)
+
+// remainder, exponetiation
+x %= 2
+x **= 2
+console.log(x)
