803000eebcda59dd60511d29f6c1225608ebadf6
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-25 20:43:49 -0300

    Fundamentar - js - for of

---
+++ 02-trilha-fundamentar/11-guia-javascript/for-of.js
@@ -0,0 +1,11 @@
+let name = 'Kleber'
+let names = ['joão', 'paulo', 'pedro']
+
+for (let char of name) {
+  console.log(char)
+}
+
+console.log('------------')
+for (let name of names) {
+  console.log(name)
+}
