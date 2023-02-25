e4c9c5daf27894633e42b4abbaf355ca57037019
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-02-25 10:25:30 -0300

    Fundamentar - js - Operadores de comparação 3

---
+++ 02-trilha-fundamentar/11-guia-javascript/oper-comparacao3.js
@@ -0,0 +1,17 @@
+let one = 1
+let two = 2
+
+// > maior que
+console.log(one > two)
+
+// >= maior igual a
+console.log(one >= 1)
+console.log(two >= 1)
+
+// < menor que
+console.log(one < two)
+
+// <= menor igual a
+console.log(one <= two)
+console.log(one <= 1)
+console.log(one <= 0)
